f37924a25e0a8486368f8a1c22f60d72ae3f6fd2
serapath <dev@serapath.de>
2018-04-29 20:13:27 +0100

    update devops to budo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ var Web3 = require('web3')
 /* ----------------------------
       WEB3 CONFIG
 ------------------------------ */
+var foo = require('foo')
+console.log(foo())
+
 
 if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
